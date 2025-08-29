371fc2949a83709aacb7947df2363fa36ec7caf2
Ketonanicolas <nainujiharsh@gmail.com>
2025-08-29 21:55:33 +0530

    back to normal

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import ProjectsPage from './projects/ProjectsPage.jsx'
-import RedirectToDiscord from './components/RedirectToDiscord.jsx' // ✅ fixed path
 import './index.css'
 
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
   { path: '/projects', element: <ProjectsPage /> },
-  { path: '/server', element: <RedirectToDiscord /> }, // 👈 Discord redirect
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
